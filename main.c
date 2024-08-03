a2b99972cc459b73bf4cf2a26f0b472d7e8d6cd5
Yakumo-zi <1477717283@qq.com>
2024-08-04 00:27:31 +0800

    feat:add line number support

--- main.c
+++ main.c
@@ -6,9 +6,9 @@ int main(int argc, char *argv[]) {
   Chunk chunk;
   init_chunk(&chunk);
   int idx = add_constants(&chunk, 1.2);
-  write_chunk(&chunk, OP_CONSTANT);
-  write_chunk(&chunk, idx);
-  write_chunk(&chunk, OP_RETURN);
+  write_chunk(&chunk, OP_CONSTANT, 123);
+  write_chunk(&chunk, idx, 123);
+  write_chunk(&chunk, OP_RETURN, 123);
   disassemble_chunk(&chunk, "test");
   free_chunk(&chunk);
   return 0;
