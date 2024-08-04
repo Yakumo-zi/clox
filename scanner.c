4cf18ada5e1eadfdc838334650b0e8990d22f98e
Yakumo-zi <1477717283@qq.com>
2024-08-04 18:54:58 +0800

    feat:add keywords support

--- scanner.c
+++ scanner.c
@@ -96,27 +96,81 @@ static Token string() {
     return make_token(TOKEN_STRING);
 }
 
-static Token number(){
-    while(isdigit(peek())){
+static Token number() {
+    while (isdigit(peek())) {
         advance();
     }
-    if(peek()=='.' && isdigit(peek_next())){
+    if (peek() == '.' && isdigit(peek_next())) {
         advance();
-        while(isdigit(peek())){
+        while (isdigit(peek())) {
             advance();
         }
     }
     return make_token(TOKEN_NUMBER);
 }
-static TokenType identifier_type(){
+
+static TokenType check_keyword(int start, int length, const char *rest,
+                               TokenType type) {
+    if (scanner.current - scanner.start == start + length &&
+        memcmp(scanner.start + start, rest, length) == 0) {
+        return type;
+    }
     return TOKEN_IDENTIFIER;
 }
-static Token identifier(){
-    while (isalpha(peek()) || isdigit(peek())) {
-        advance(); 
- 
+
+static TokenType identifier_type() {
+    switch (scanner.start[0]) {
+    case 'a':
+        return check_keyword(1, 2, "nd", TOKEN_AND);
+    case 'c':
+        return check_keyword(1, 4, "lass", TOKEN_CLASS);
+    case 'e':
+        return check_keyword(1, 3, "lse", TOKEN_ELSE);
+    case 'i':
+        return check_keyword(1, 1, "f", TOKEN_IF);
+    case 'n':
+        return check_keyword(1, 2, "il", TOKEN_NIL);
+    case 'o':
+        return check_keyword(1, 1, "r", TOKEN_OR);
+    case 'p':
+        return check_keyword(1, 4, "rint", TOKEN_PRINT);
+    case 'r':
+        return check_keyword(1, 5, "eturn", TOKEN_RETURN);
+    case 's':
+        return check_keyword(1, 4, "uper", TOKEN_SUPER);
+    case 'v':
+        return check_keyword(1, 2, "ar", TOKEN_VAR);
+    case 'w':
+        return check_keyword(1, 4, "hile", TOKEN_WHILE);
+    case 'f':
+        if (scanner.current - scanner.start > 1) {
+            switch (scanner.start[1]) {
+            case 'a':
+                return check_keyword(2, 3, "lse", TOKEN_FALSE);
+            case 'o':
+                return check_keyword(2, 1, "r", TOKEN_FOR);
+            case 'u':
+                return check_keyword(2, 1, "n", TOKEN_FUN);
+            }
+        }
+        break;
+    case 't':
+      if (scanner.current - scanner.start > 1) {
+        switch (scanner.start[1]) {
+          case 'h': return check_keyword(2, 2, "is", TOKEN_THIS);
+          case 'r': return check_keyword(2, 2, "ue", TOKEN_TRUE);
+        }
+      }
+      break;
     }
-   return make_token(identifier_type());
+    return TOKEN_IDENTIFIER;
+}
+
+static Token identifier() {
+    while (isalpha(peek()) || isdigit(peek())) {
+        advance();
+    }
+    return make_token(identifier_type());
 }
 Token scan_token() {
     skip_white_space();
@@ -124,10 +178,10 @@ Token scan_token() {
     if (is_at_end())
         return make_token(TOKEN_EOF);
     char c = advance();
-    if(isalpha(c)){
+    if (isalpha(c)) {
         return identifier();
     }
-    if(isdigit(c)){
+    if (isdigit(c)) {
         return number();
     }
     switch (c) {
